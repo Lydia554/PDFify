68d1fefd28df5ad97e98dbd09442e74796e24f0c
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-05-06 21:52:42 +0200

    Update

--- app/server/models/User.js
+++ app/server/models/User.js
@@ -51,27 +51,17 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-
 userSchema.pre("save", async function (next) {
-  if (this.isModified("apiKey")) {
-    this.apiKey = encrypt(this.apiKey);
-  }
-  next();
-});
-
-
-userSchema.methods.getDecryptedApiKey = function () {
-  return decrypt(this.apiKey);
-};
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
-
   try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+    if (this.isModified("apiKey")) {
+      this.apiKey = encrypt(this.apiKey); 
+    }
+
+    if (this.isModified("password")) {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt); 
+    }
+
     next();
   } catch (error) {
     next(error);
