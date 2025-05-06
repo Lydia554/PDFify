31e2f0ead0a938e9c3bf65b7c4a45cef82381f15
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-05-06 23:05:13 +0200

    Update

--- app/server/models/User.js
+++ app/server/models/User.js
@@ -38,7 +38,7 @@ function decrypt(text) {
 const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
-    apiKey: { type: String, unique: true },
+    apiKey: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     usageCount: { type: Number, default: 0 },
     maxUsage: { type: Number, default: 30 },
