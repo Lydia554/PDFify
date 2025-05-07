bcd6c1ce490890c48e8fb2efd9df669a01809735
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-05-07 20:11:57 +0200

    Update

--- app/server/models/User.js
+++ app/server/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
-const dotenv = require("dotenv"); 
-dotenv.config(); 
+const dotenv = require("dotenv");
+dotenv.config();
 
 const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
 if (!ENCRYPTION_KEY || ENCRYPTION_KEY.length !== 32) {
@@ -12,21 +12,19 @@ if (!ENCRYPTION_KEY || ENCRYPTION_KEY.length !== 32) {
 const IV_LENGTH = 16;
 
 function encrypt(text) {
-  const iv = crypto.randomBytes(IV_LENGTH); 
+  const iv = crypto.randomBytes(IV_LENGTH);
   const cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
   let encrypted = cipher.update(text, "utf8");
   encrypted = Buffer.concat([encrypted, cipher.final()]);
-  return iv.toString("hex") + ":" + encrypted.toString("hex"); 
+  return iv.toString("hex") + ":" + encrypted.toString("hex");
 }
 
 function decrypt(text) {
   const textParts = text.split(":");
-  if (textParts.length !== 2) {
-    throw new Error("Invalid encrypted text format");
-  }
+  if (textParts.length !== 2) throw new Error("Invalid encrypted text format");
 
-  const iv = Buffer.from(textParts[0], "hex"); 
-  const encryptedText = Buffer.from(textParts[1], "hex"); 
+  const iv = Buffer.from(textParts[0], "hex");
+  const encryptedText = Buffer.from(textParts[1], "hex");
   const decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
   let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
@@ -49,17 +47,20 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Method to get decrypted API key
 userSchema.methods.getDecryptedApiKey = function () {
   return decrypt(this.apiKey);
 };
 
+// Method to change API key manually
+userSchema.methods.setNewApiKey = function (rawApiKey) {
+  this.apiKey = encrypt(rawApiKey);
+};
+
+// Method to change password manually
 userSchema.methods.setNewPassword = async function (newPassword) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(newPassword, salt);
-};
-
-userSchema.methods.setNewApiKey = function (rawApiKey) {
-  this.apiKey = encrypt(rawApiKey);
 };
 
 module.exports = mongoose.model("User", userSchema);
