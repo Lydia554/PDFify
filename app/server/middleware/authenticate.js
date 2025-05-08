310107a9c88ee91cfc34195b120cc19ba6945a77
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-05-08 18:05:38 +0200

    Update

--- app/server/middleware/authenticate.js
+++ app/server/middleware/authenticate.js
@@ -10,6 +10,7 @@ const authenticate = async (req, res, next) => {
     apiKey = authHeader.split(" ")[1];
   }
 
+
   if (!apiKey) {
     apiKey = req.query.apiKey;
   }
@@ -21,6 +22,7 @@ const authenticate = async (req, res, next) => {
   }
 
   try {
+    
     const users = await User.find();
     console.log("👥 [Auth] Total users:", users.length);
 
@@ -42,16 +44,18 @@ const authenticate = async (req, res, next) => {
 
     console.log("✅ [Auth] User authenticated:", user.email);
 
-    // ✅ Attach the full user object to req.user
-    req.user = user;
+    req.user = {
+      userId: user._id,
+      email: user.email,
+    };
 
-    // You can also create a sanitized version of the data if needed
     req.userData = {
       email: user.email,
-      apiKey: user.getDecryptedApiKey(), // if you need this specific value elsewhere
+      apiKey: user.getDecryptedApiKey(),
       usageCount: user.usageCount,
       maxUsage: user.maxUsage,
       isPremium: user.isPremium,
+      userId: user._id,
     };
 
     next();
