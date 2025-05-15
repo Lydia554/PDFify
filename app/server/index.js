6ed1c9cb280db78b5da450fcf82210667b4bb438
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-05-15 21:18:35 +0200

    Update

--- app/server/index.js
+++ app/server/index.js
@@ -58,6 +58,7 @@ app.use("/api", recipeRoutes);
 app.use("/api", shopOrderRoutes);
 app.use("/api", therapyReportRoutes);
 app.use("/api", htmlRoutes);
+app.use("/friendly", require("./routes/friendly"));
 app.use("/api/", packingSlipRoutes);
 app.use("/api/stripe/webhook", stripeRoutes); 
 app.use("/api/stripe", paymentRoutes);
