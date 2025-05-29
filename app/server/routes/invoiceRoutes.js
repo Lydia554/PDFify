1402d61aa4b0c0183e50e36a768f88f421fe3367
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-05-29 21:34:28 +0200

    Update

--- app/server/routes/invoiceRoutes.js
+++ app/server/routes/invoiceRoutes.js
@@ -142,7 +142,7 @@ function generateInvoiceHTML(data) {
             bottom: 0;
             left: 0;
             width: 100%;
-            height: 60px;
+            height: 80px;
             padding: 10px 20px;
             background-color: #f9f9f9;
             color: #444;
