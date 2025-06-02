8099b862e64c0292cc81a17b99e1d5d9b188b62c
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-06-02 17:08:16 +0200

    Update

--- app/server/routes/invoiceRoutes.js
+++ app/server/routes/invoiceRoutes.js
@@ -176,7 +176,7 @@ return `
       .footer {
         position: static;
         max-width: 800px;
-        margin: 110px auto;
+        margin: 100px auto 40px auto;
         padding: 10px 20px;
         background-color: #f0f2f7;
         color: #555;
@@ -186,6 +186,7 @@ return `
         font-size: 11px;
         border-radius: 0 0 16px 16px;
         box-sizing: border-box;
+       
       }
 
       .footer p {
