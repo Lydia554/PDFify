7a0796c534624da4206684add3828e811fdd40c1
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-06-05 18:47:17 +0200

    Update

--- app/server/routes/shopifyRoutes.js
+++ app/server/routes/shopifyRoutes.js
@@ -248,7 +248,10 @@ router.post('/invoice', async (req, res) => {
     </html>
     `;
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
+      
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
     const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
