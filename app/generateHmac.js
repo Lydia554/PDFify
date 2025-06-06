626f605ae2837c7af79154fdb5c94ce3e5514f24
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-06-06 20:17:47 +0200

    Update

--- app/generateHmac.js
+++ app/generateHmac.js
@@ -4,14 +4,14 @@ const crypto = require('crypto');
 
 const secret = 'a514125f3e4f3cbaac61103f1a05aed423b8c079b11d8d18f8faac1430589fd5'; // your actual secret
 const body = JSON.stringify({
-  id: 123456789,
-  email: "customer@example.com",
-  created_at: "2025-06-06T10:00:00-04:00",
-  line_items: [
-    { name: "Sample Product", quantity: 1, price: "19.99" }
+  "id": 123456789,
+  "email": "customer@example.com",
+  "created_at": "2025-06-06T10:00:00-04:00",
+  "line_items": [
+    { "name": "Sample Product", "quantity": 1, "price": "19.99" }
   ]
-  // exactly the same JSON you send in Postman body!
-});
+}
+);
 
 const hmac = crypto.createHmac('sha256', secret).update(body, 'utf8').digest('base64');
 
