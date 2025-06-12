749da03d7d11ca4334b72587ced8aa10fe330ad1
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-06-12 18:29:21 +0200

    Update

--- app/server/routes/shopifyWebhookRoutes.js
+++ app/server/routes/shopifyWebhookRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const crypto = require("crypto");
+
 const axios = require("axios");
 const router = express.Router();
 
