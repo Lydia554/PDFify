07d22363454363e31a2a1888c1d8d98014a93874
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-06-24 17:19:11 +0200

    Update

--- app/server/routes/authRoutes.js
+++ app/server/routes/authRoutes.js
@@ -40,7 +40,7 @@ router.post("/login", async (req, res) => {
       return res.status(401).json({ error: "Invalid password" });
     }
 
-    // Set session
+   
     req.session.userId = user._id;
 
     res.json({
