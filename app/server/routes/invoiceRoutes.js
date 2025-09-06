028d83669261906b76d514f7727505dfc2546f7a
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-09-06 12:50:22 +0200

    Update

--- app/server/routes/invoiceRoutes.js
+++ app/server/routes/invoiceRoutes.js
@@ -26,9 +26,8 @@ const FORCE_PLAN = process.env.FORCE_PLAN;
 router.post("/generate-invoice", authenticate, dualAuth, async (req, res) => {
   console.log("🌐 /generate-invoice router hit");
 
-process.env.ICC_PROFILE_PATH = path.resolve(__dirname, "sRGB_v4_ICC_preference.icc");
 
-const iccPath = path.resolve(__dirname, "sRGB_v4_ICC_preference.icc");
+const iccPath = path.resolve(__dirname, "../routes/sRGB_v4_ICC_preference.icc");
 const gsIccPath = iccPath.replace(/\\/g, "/");
 
 console.log("🔍 Using ICC profile path:", iccPath);
