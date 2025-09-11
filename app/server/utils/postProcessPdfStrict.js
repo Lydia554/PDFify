e53b125af872b3e5b1cf6920b3de1df0534f8b92
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-09-11 18:41:11 +0200

    Update

--- app/server/utils/postProcessPdfStrict.js
+++ app/server/utils/postProcessPdfStrict.js
@@ -1,14 +1,20 @@
+// server/utils/postProcessPdfStrict.js
 const { PDFDocument, PDFName, PDFString, PDFArray } = require('pdf-lib');
 const fs = require('fs');
 
 async function postProcessPdfStrict(pdfBytes, zugferdXml = null, localeMeta = {}) {
   const pdfDoc = await PDFDocument.load(pdfBytes);
 
-  // --- Preserve existing /OutputIntents ---
+  // --- Preserve /OutputIntents and force /S = GTS_PDFA1 ---
   const existingOutputIntentsRef = pdfDoc.catalog.get(PDFName.of('OutputIntents'));
-  let outputIntents = null;
   if (existingOutputIntentsRef) {
-    outputIntents = pdfDoc.context.lookup(existingOutputIntentsRef, PDFArray);
+    const outputIntents = pdfDoc.context.lookup(existingOutputIntentsRef, PDFArray);
+    outputIntents.forEach(intentRef => {
+      const intent = pdfDoc.context.lookup(intentRef);
+      intent.set(PDFName.of('S'), PDFName.of('GTS_PDFA1')); // Force correct name
+    });
+    pdfDoc.catalog.set(PDFName.of('OutputIntents'), outputIntents);
+    console.log('🎨 /OutputIntents checked and /S forced to GTS_PDFA1');
   }
 
   // --- Attach ZUGFeRD XML if provided ---
@@ -49,11 +55,13 @@ async function postProcessPdfStrict(pdfBytes, zugferdXml = null, localeMeta = {}
   const xmpData = `<?xpacket begin='' id='W5M0MpCehiHzreSzNTczkc9d'?>
 <x:xmpmeta xmlns:x='adobe:ns:meta/' x:xmptk='PDF-Lib'>
   <rdf:RDF xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'>
-    <rdf:Description rdf:about=''
-      xmlns:pdfaid='http://www.aiim.org/pdfa/ns/id/'
-      xmlns:dc='http://purl.org/dc/elements/1.1/'>
+    <!-- PDF/A identification -->
+    <rdf:Description rdf:about='' xmlns:pdfaid='http://www.aiim.org/pdfa/ns/id/'>
       <pdfaid:part>3</pdfaid:part>
       <pdfaid:conformance>B</pdfaid:conformance>
+    </rdf:Description>
+    <!-- Document info -->
+    <rdf:Description rdf:about='' xmlns:dc='http://purl.org/dc/elements/1.1/'>
       <dc:title>${title}</dc:title>
       <dc:creator>${creator}</dc:creator>
       <dc:language>${language}</dc:language>
@@ -65,12 +73,6 @@ async function postProcessPdfStrict(pdfBytes, zugferdXml = null, localeMeta = {}
   const xmpStream = pdfDoc.context.register(pdfDoc.context.stream(Buffer.from(xmpData, 'utf8')));
   pdfDoc.catalog.set(PDFName.of('Metadata'), xmpStream);
   console.log('📄 PDF/A-3b XMP metadata injected');
-
-  // --- Restore /OutputIntents if it existed ---
-  if (outputIntents) {
-    pdfDoc.catalog.set(PDFName.of('OutputIntents'), outputIntents);
-    console.log('🎨 /OutputIntents preserved');
-  }
 
   return await pdfDoc.save({ useObjectStreams: false });
 }
