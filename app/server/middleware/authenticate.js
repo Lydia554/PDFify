9e45794c368e4bded05061e6564dc2449da408ed
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-09-30 21:19:17 +0200

    Update

--- app/server/middleware/authenticate.js
+++ app/server/middleware/authenticate.js
@@ -52,6 +52,7 @@ if (!user || user.deleted) {
       maxUsage: user.maxUsage,
       isPremium: user.isPremium,
        planType: user.planType || "Free", 
+       shopDomain: user.shopDomain || user.email 
     };
 
    
