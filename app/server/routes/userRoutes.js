40918e4aa10cb8c67a5e3dc2cdaed13837943c67
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-11-08 21:45:49 +0100

    Update

--- app/server/routes/userRoutes.js
+++ app/server/routes/userRoutes.js
@@ -5,6 +5,7 @@ const authenticate = require("../middleware/authenticate");
 const dualAuth = require("../middleware/dualAuth");
 const sendEmail = require("../sendEmail");
 const ShopConfig = require("../models/ShopConfig"); 
+const crypto = require("crypto");
 
 const router = express.Router();
 
