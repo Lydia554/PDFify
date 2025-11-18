7328dc510ab3974a8013185108d738c7c92372e8
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-11-18 13:34:09 +0100

    Update

--- app/server/Helpers/com/yourcompany/PdfA3bFixer.java
+++ app/server/Helpers/com/yourcompany/PdfA3bFixer.java
@@ -2,17 +2,16 @@ package com.yourcompany;
 
 import java.io.File;
 import java.io.IOException;
-import org.apache.pdfbox.preflight.PreflightDocument;
-import org.apache.pdfbox.preflight.Format;
-import org.apache.pdfbox.preflight.parser.PreflightParser;
+
 import org.apache.pdfbox.preflight.ValidationResult;
-import org.apache.pdfbox.preflight.exception.SyntaxValidationException;
+import org.apache.pdfbox.preflight.ValidationResult.ValidationError;
 import org.apache.pdfbox.preflight.exception.ValidationException;
-import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.preflight.parser.PreflightParser;
+import org.apache.pdfbox.preflight.PreflightDocument;
 
 public class PdfA3bFixer {
     public static void main(String[] args) throws IOException {
-        if(args.length != 2) {
+        if (args.length != 2) {
             System.err.println("Usage: PdfA3bFixer <input.pdf> <output.pdf>");
             System.exit(1);
         }
@@ -20,44 +19,33 @@ public class PdfA3bFixer {
         File inputFile = new File(args[0]);
         File outputFile = new File(args[1]);
 
-        ValidationResult result = null;
+        PreflightParser parser = new PreflightParser(inputFile);
         PreflightDocument preflightDoc = null;
 
         try {
-            PreflightParser parser = new PreflightParser(inputFile);
-            // Parse and cast to PreflightDocument (PDFBox 3.x API)
-            // Note: Preflight only supports PDF/A-1B and PDF/A-1A
-            PDDocument doc = parser.parse(Format.PDF_A1B);
-            preflightDoc = (PreflightDocument) doc;
+            parser.parse();
+            preflightDoc = parser.getPreflightDocument();
 
-            // Validate and get result
-            result = preflightDoc.validate();
+            preflightDoc.validate();
+            ValidationResult result = preflightDoc.getResult();
 
-            if(!result.isValid()) {
+            if (!result.isValid()) {
                 System.err.println("PDF/A validation errors:");
-                for (ValidationResult.ValidationError error : result.getErrorsList()) {
+                for (ValidationError error : result.getErrorsList()) {
                     System.err.println(error.getErrorCode() + " : " + error.getDetails());
                 }
 
                 // Fix missing ID
-                if(preflightDoc.getDocumentCatalog().getCOSObject().getItem("ID") == null) {
-                    preflightDoc.getDocumentCatalog().getCOSObject().setItem("ID",
-                        preflightDoc.getDocument().getTrailer().getItem("ID"));
+                if (preflightDoc.getDocument().getTrailer().getCOSObject().getItem("ID") == null) {
+                    preflightDoc.getDocument().getTrailer().getCOSObject().setItem(
+                        "ID",
+                        preflightDoc.getDocument().getDocumentID()
+                    );
                 }
-                // TODO: embed fonts properly, fix XMP metadata
             }
 
             preflightDoc.save(outputFile);
 
-        } catch (SyntaxValidationException e) {
-            System.err.println("Syntax validation failed: " + e.getMessage());
-            result = e.getResult();
-            if (result != null) {
-                for (ValidationResult.ValidationError error : result.getErrorsList()) {
-                    System.err.println(error.getErrorCode() + " : " + error.getDetails());
-                }
-            }
-            System.exit(1);
         } catch (ValidationException e) {
             System.err.println("Validation failed: " + e.getMessage());
             System.exit(1);
